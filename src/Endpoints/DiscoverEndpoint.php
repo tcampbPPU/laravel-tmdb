5f63d3d89df96d88262e3df42a3e145699d2715a
Tanner Campbell <tanner@rimsys.io>
2021-12-07 23:25:05 -0500

    extract to trait

--- src/Endpoints/DiscoverEndpoint.php
+++ src/Endpoints/DiscoverEndpoint.php
@@ -2,14 +2,15 @@
 
 namespace Tcamp\Tmdb\Endpoints;
 
-use Carbon\Carbon;
 use Tcamp\Tmdb\Api;
 use Tcamp\Tmdb\Collections\MovieCollection;
-use Tcamp\Tmdb\Exceptions\IncorrectValueException;
 use Tcamp\Tmdb\Models\Pagination;
+use Tcamp\Tmdb\Traits\QueryBuilder;
 
 class DiscoverEndpoint
 {
+    use QueryBuilder;
+
     protected string $sort_by = 'popularity.desc';
     protected ?string $language = null;
     protected ?string $region = null;
@@ -27,6 +28,30 @@ class DiscoverEndpoint
     public function __construct(
         protected Api $api
     ) {
+    }
+
+    /**
+     * Dump query
+     *
+     * @return array
+     */
+    public function query(): array
+    {
+        return collect([
+            'page' => $this->page,
+            'language' => $this->language,
+            'region' => $this->region,
+            'sort_by' => $this->sort_by,
+            'certification' => $this->certification,
+            'certification_country' => $this->certification_country,
+            'certification_lte' => $this->certification_lte,
+            'certification_gte' => $this->certification_gte,
+            'include_adult' => $this->include_adult,
+            'include_video' => $this->include_video,
+            'primary_release_year' => $this->primary_release_year,
+        ])
+            ->whereNotNull()
+            ->toArray();
     }
 
     /**
@@ -67,227 +92,5 @@ class DiscoverEndpoint
         $data['items'] = new MovieCollection(data_get($response, 'results') ?? []);
 
         return new Pagination(...$data);
-    }
-
-    /**
-     * Dump query
-     *
-     * @return array
-     */
-    public function query(): array
-    {
-        return collect([
-            'page' => $this->page,
-            'language' => $this->language,
-            'region' => $this->region,
-            'sort_by' => $this->sort_by,
-            'certification' => $this->certification,
-            'certification_country' => $this->certification_country,
-            'certification_lte' => $this->certification_lte,
-            'certification_gte' => $this->certification_gte,
-            'include_adult' => $this->include_adult,
-            'include_video' => $this->include_video,
-            'primary_release_year' => $this->primary_release_year,
-        ])
-            ->whereNotNull()
-            ->toArray();
-    }
-
-    /**
-     * Set starting page
-     *
-     * @param int $page
-     * @return $this
-     */
-    public function page(int $page): self
-    {
-        $this->page = $page;
-
-        return $this;
-    }
-
-    /**
-     * Specify a language to query translatable fields with
-     *
-     * @param string $lang
-     * @return self
-     */
-    public function language(string $lang = 'em-US'): self
-    {
-        $matches = (bool) preg_match("/([a-z]{2})-([A-Z]{2})/", $lang);
-
-        if (! $matches) {
-            throw new IncorrectValueException('Pattern must match the following pattern: ([a-z]{2})-([A-Z]{2})');
-        }
-
-        $this->language = $lang;
-
-        return $this;
-    }
-
-    /**
-     * Specify a ISO 3166-1 code to filter release dates. Must be uppercase.
-     *
-     * @param string $code
-     * @return self
-     */
-    public function region(string $code = 'US'): self
-    {
-        $matches = (bool) preg_match("/^[A-Z]{2}$/", $code);
-
-        if (! $matches) {
-            throw new IncorrectValueException('Pattern must match the following pattern: ([A-Z]{2})');
-        }
-
-        $this->region = $code;
-
-        return $this;
-    }
-
-    /**
-     * Set sort by options.
-     *
-     * @param string $value
-     * @return self
-     */
-    public function sort(string $value): self
-    {
-        $allowed = [
-            'popularity.asc',
-            'popularity.desc',
-            'release_date.asc',
-            'release_date.desc',
-            'revenue.asc',
-            'revenue.desc',
-            'primary_release_date.asc',
-            'primary_release_date.desc',
-            'original_title.asc',
-            'original_title.desc',
-            'vote_average.asc',
-            'vote_average.desc',
-            'vote_count.asc',
-            'vote_count.desc',
-        ];
-
-        if (! in_array($value, $allowed)) {
-            throw new IncorrectValueException('Incorrect sort by value specified');
-        }
-
-        $this->sort_by = $value;
-
-        return $this;
-    }
-
-    /**
-     *
-     * @param string $certification
-     * @return self
-     */
-    public function certification(string $certification): self
-    {
-        $this->certification = $certification;
-
-        return $this;
-    }
-
-    /**
-     *
-     * @param string $country
-     * @return self
-     */
-    public function certificationCountry(string $country): self
-    {
-        $this->certification_country = $country;
-
-        return $this;
-    }
-
-    /**
-     *
-     * @param string $country
-     * @return self
-     */
-    public function certificationCountryLte(string $country): self
-    {
-        $this->certification_lte = $country;
-
-        return $this;
-    }
-
-    /**
-     *
-     * @param string $country
-     * @return self
-     */
-    public function certificationCountryGte(string $country): self
-    {
-        $this->certification_gte = $country;
-
-        return $this;
-    }
-
-    /**
-     * A filter and include or exclude adult movie
-     *
-     * @param bool $val
-     * @return $this
-     */
-    public function includeAdult(bool $val = true): self
-    {
-        $this->include_adult = $val;
-
-        return $this;
-    }
-
-    /**
-     * A filter to include or exclude videos
-     *
-     * @param bool $val
-     * @return $this
-     */
-    public function includeVideo(bool $val = true): self
-    {
-        $this->include_video = $val;
-
-        return $this;
-    }
-
-    /**
-     * A filter to limit the results to a specific primary release year.
-     *
-     * @param int $year
-     * @return self
-     */
-    public function releaseYear(int $year): self
-    {
-        $this->primary_release_year = $year;
-
-        return $this;
-    }
-
-    /**
-     * Filter and only include movies that have a primary release date that is greater or equal to the specified value.
-     *
-     * @param string $date
-     * @return self
-     */
-    public function releaseDateGte(string $date): self
-    {
-        $this->primary_release_date_gte = Carbon::parse($date)->format('Y-m-d');
-
-        return $this;
-    }
-
-    /**
-     * A filter to limit the results to a specific primary release year.
-     *
-     * @param string $date
-     * @return self
-     */
-    public function releaseDateLte(string $date): self
-    {
-        $this->primary_release_date_lte = Carbon::parse($date)->format('Y-m-d');
-
-        return $this;
     }
 }
--- src/Traits/QueryBuilder.php
+++ src/Traits/QueryBuilder.php
@@ -2,6 +2,7 @@
 
 namespace Tcamp\Tmdb\Traits;
 
+use Carbon\Carbon;
 use Tcamp\Tmdb\Exceptions\IncorrectValueException;
 
 trait QueryBuilder
@@ -10,10 +11,14 @@ trait QueryBuilder
      * Set search query
      *
      * @param int $page
-     * @return $this
+     * @return self
      */
-    public function page(int $page): self
+    public function page(int $page): ?self
     {
+        if (! property_exists($this, 'page')) {
+            return null;
+        }
+
         $this->page = $page;
 
         return $this;
@@ -23,10 +28,14 @@ trait QueryBuilder
      * Set starting page
      *
      * @param string $search
-     * @return $this
+     * @return self
      */
-    public function search(string $search): self
+    public function search(string $search): ?self
     {
+        if (! property_exists($this, 'query')) {
+            return null;
+        }
+
         $this->query = $search;
 
         return $this;
@@ -38,8 +47,12 @@ trait QueryBuilder
      * @param string $lang
      * @return self
      */
-    public function language(string $lang = 'em-US'): self
+    public function language(string $lang = 'em-US'): ?self
     {
+        if (! property_exists($this, 'language')) {
+            return null;
+        }
+
         $matches = (bool) preg_match("/([a-z]{2})-([A-Z]{2})/", $lang);
 
         if (! $matches) {
@@ -57,8 +70,12 @@ trait QueryBuilder
       * @param string $code
       * @return self
       */
-    public function region(string $code = 'US'): self
+    public function region(string $code = 'US'): ?self
     {
+        if (! property_exists($this, 'region')) {
+            return null;
+        }
+
         $matches = (bool) preg_match("/^[A-Z]{2}$/", $code);
 
         if (! $matches) {
@@ -74,10 +91,14 @@ trait QueryBuilder
      * A filter and include or exclude adult movie
      *
      * @param bool $val
-     * @return $this
+     * @return self
      */
-    public function includeAdult(bool $val = true): self
+    public function includeAdult(bool $val = true): ?self
     {
+        if (! property_exists($this, 'include_adult')) {
+            return null;
+        }
+
         $this->include_adult = $val;
 
         return $this;
@@ -89,8 +110,12 @@ trait QueryBuilder
      * @param int $year
      * @return self
      */
-    public function year(int $year): self
+    public function year(int $year): ?self
     {
+        if (! property_exists($this, 'year')) {
+            return null;
+        }
+
         $this->year = $year;
 
         return $this;
@@ -102,8 +127,12 @@ trait QueryBuilder
      * @param int $year
      * @return self
      */
-    public function releaseYear(int $year): self
+    public function releaseYear(int $year): ?self
     {
+        if (! property_exists($this, 'primary_release_year')) {
+            return null;
+        }
+
         $this->primary_release_year = $year;
 
         return $this;
@@ -115,9 +144,166 @@ trait QueryBuilder
      * @param int $year
      * @return self
      */
-    public function firstAirDate(int $year): self
+    public function firstAirDate(int $year): ?self
     {
+        if (! property_exists($this, 'first_air_date_year')) {
+            return null;
+        }
+
         $this->first_air_date_year = $year;
+
+        return $this;
+    }
+
+    /**
+     * Set sort by options.
+     *
+     * @param string $value
+     * @return self
+     */
+    public function sort(string $value): ?self
+    {
+        if (! property_exists($this, 'sort_by')) {
+            return null;
+        }
+
+        $allowed = [
+            'popularity.asc',
+            'popularity.desc',
+            'release_date.asc',
+            'release_date.desc',
+            'revenue.asc',
+            'revenue.desc',
+            'primary_release_date.asc',
+            'primary_release_date.desc',
+            'original_title.asc',
+            'original_title.desc',
+            'vote_average.asc',
+            'vote_average.desc',
+            'vote_count.asc',
+            'vote_count.desc',
+        ];
+
+        if (! in_array($value, $allowed)) {
+            throw new IncorrectValueException('Incorrect sort by value specified');
+        }
+
+        $this->sort_by = $value;
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param string $certification
+     * @return self
+     */
+    public function certification(string $certification): ?self
+    {
+        if (! property_exists($this, 'certification')) {
+            return null;
+        }
+
+        $this->certification = $certification;
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param string $country
+     * @return self
+     */
+    public function certificationCountry(string $country): ?self
+    {
+        if (! property_exists($this, 'certification_country')) {
+            return null;
+        }
+
+        $this->certification_country = $country;
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param string $country
+     * @return self
+     */
+    public function certificationCountryLte(string $country): ?self
+    {
+        if (! property_exists($this, 'certification_lte')) {
+            return null;
+        }
+
+        $this->certification_lte = $country;
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param string $country
+     * @return self
+     */
+    public function certificationCountryGte(string $country): ?self
+    {
+        if (! property_exists($this, 'certification_gte')) {
+            return null;
+        }
+
+        $this->certification_gte = $country;
+
+        return $this;
+    }
+
+    /**
+     * A filter to include or exclude videos
+     *
+     * @param bool $val
+     * @return $this
+     */
+    public function includeVideo(bool $val = true): ?self
+    {
+        if (! property_exists($this, 'include_video')) {
+            return null;
+        }
+
+        $this->include_video = $val;
+
+        return $this;
+    }
+
+    /**
+     * Filter and only include movies that have a primary release date that is greater or equal to the specified value.
+     *
+     * @param string $date
+     * @return self
+     */
+    public function releaseDateGte(string $date): ?self
+    {
+        if (! property_exists($this, 'primary_release_date_gte')) {
+            return null;
+        }
+
+        $this->primary_release_date_gte = Carbon::parse($date)->format('Y-m-d');
+
+        return $this;
+    }
+
+    /**
+     * A filter to limit the results to a specific primary release year.
+     *
+     * @param string $date
+     * @return self
+     */
+    public function releaseDateLte(string $date): ?self
+    {
+        if (! property_exists($this, 'primary_release_date_lte')) {
+            return null;
+        }
+
+        $this->primary_release_date_lte = Carbon::parse($date)->format('Y-m-d');
 
         return $this;
     }
