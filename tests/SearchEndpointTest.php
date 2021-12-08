90c92093fbe3e1cdaaebfd109b38bd32dce42e06
Tanner Campbell <tanner@rimsys.io>
2021-12-07 23:05:49 -0500

    pass test

--- tests/SearchEndpointTest.php
+++ tests/SearchEndpointTest.php
@@ -3,13 +3,17 @@
 use function PHPUnit\Framework\assertInstanceOf;
 
 use Tcamp\Tmdb\Collections\MovieCollection;
+use Tcamp\Tmdb\Models\Movie;
+use Tcamp\Tmdb\Models\Pagination;
 
 test('search movie by title', function () {
     httpClient()->fake([
         'https://api.themoviedb.org/3/*' => httpClient()->response(recentMovieDataset(), 200),
     ]);
 
-    $searchResults = tmdb()->search('dune')->get();
+    $searchResults = tmdb()->search()->movies('dune');
 
-    assertInstanceOf(MovieCollection::class, $searchResults);
+    assertInstanceOf(Pagination::class, $searchResults);
+    assertInstanceOf(MovieCollection::class, $searchResults->items);
+    assertInstanceOf(Movie::class, $searchResults->items[0]);
 });
